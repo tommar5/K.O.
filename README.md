a0a7d7f555e7e6b11132b6733bba0cb017ca4d11
tommar5 <nikop991@gmail.com>
2017-12-08 17:24:08 +0200

    Commit one

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# K.O.
+## Installation
+
+cp docker-compose.yml.dist docker-compose.yml
+docker-compose up -d
+dcc php
+php app/console assets:install httpdocs
+
+## Building release
+
+bin/build
+
+## Testing
+
+    bin/phpunit -c app
+
+### Behat
+
+See [behat3](http://docs.behat.org/en/latest/) for reference.
+To run behat tests:
+
+    bin/reload test
+    bin/behat
+
+### Reload
+
+**reload** - reloads your application datasources in the order: drop database(if available), create database, run migrations,
+load fixtures, clear and warmup cache. These binaries are located in **app/Resources/bin** and may be adapted
+for custom usage.
+
+    bin/reload test
+
+Would reload application for **test** environment. Default is **dev** as usual in symfony2 application.
+
--- app/AppKernel.php
+++ app/AppKernel.php
@@ -5,21 +5,36 @@ use Symfony\Component\Config\Loader\LoaderInterface;
 
 class AppKernel extends Kernel
 {
+    public function __construct($environment, $debug)
+    {
+        date_default_timezone_set('Europe/Vilnius');
+
+        parent::__construct($environment, $debug);
+    }
+
     public function registerBundles()
     {
-        $bundles = array(
+        $bundles = [
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new DataDog\PagerBundle\DataDogPagerBundle(),
+            new DataDog\AuditBundle\DataDogAuditBundle(),
+            new EWZ\Bundle\RecaptchaBundle\EWZRecaptchaBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Vich\UploaderBundle\VichUploaderBundle(),
+            new Liip\ImagineBundle\LiipImagineBundle(),
             new AppBundle\AppBundle(),
-        );
+            new CoreSphere\ConsoleBundle\CoreSphereConsoleBundle(),
+            new Stfalcon\Bundle\TinymceBundle\StfalconTinymceBundle(),
+        ];
 
-        if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
+        if (in_array($this->getEnvironment(), ['dev', 'test'])) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
@@ -31,6 +46,11 @@ class AppKernel extends Kernel
 
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
+        $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
+    }
+
+    public function getLogDir()
+    {
+        return $this->rootDir . '/../logs';
     }
 }
